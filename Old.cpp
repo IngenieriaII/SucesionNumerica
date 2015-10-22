0f0d782f5d1514f55515591d5d800bdd9485a231
josevs99 <josevs99@hotmail.com>
2015-10-22 00:37:15 -0500

    Codigo inicial
    Código inicial, donde se despliega secuencia de numeros

---
+++ Old.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "1";
+    cout << endl;
+    cout << "2";
+    cout << endl;
+    cout << "3";
+    cout << endl;
+    cout << "4";
+    cout << endl;
+    cout << "5";
+    cout << endl;
+    cout << "6";
+    cout << endl;
+    cout << "7";
+    cout << endl;
+    cout << "8";
+    cout << endl;
+    cout << "9";
+    cout << endl;
+    cout << "10";
+    cout << endl;
+}
